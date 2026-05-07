252cb52e9820940412513ab55f52b3fdc1e52aa3
curiouscosmos <onecuriouscosmos@gmail.com>
2026-05-06 20:28:29 -0700

    Reduced Chunk size for TTS

--- Anything Reader/ReaderPlaybackChunkService.swift
+++ Anything Reader/ReaderPlaybackChunkService.swift
@@ -10,8 +10,8 @@ import Foundation
 import SwiftData
 
 struct ReaderPlaybackChunkService {
-    static let initialChunkLength = 120
-    static let preferredChunkLength = 240
+    static let initialChunkLength = 40
+    static let preferredChunkLength = 120
     static let prefetchChunkCount = 3
     static let pdfPageBreakMarker = "[[PDF_PAGE_BREAK]]"
     static let epubChapterBreakMarker = "[[EPUB_CHAPTER_BREAK]]"
